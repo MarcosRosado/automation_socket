c50d1ba7985252ccb2ce93b86dae2ea14d26e587
MarcosRosado <marcos@tacticup.com.br>
2022-08-02 22:35:26 -0300

    added on route

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const WebSocket = require('ws')
+
+const socket = new WebSocket('ws://3.221.144.175');
 
 const app = express();
 
@@ -14,8 +17,18 @@ app.use(express.json());
 
 app.use(morgan('dev'));
 
-app.get('/login', (req, res, next) => {
-    res.json({ token: '123456' });
+app.get('/pump/on', (req, res, next) => {
+    // Create a new WebSocket.
+    try{
+        socket.send( "TURN_ON_PUMP");
+        res.json({ res: 'PUMP_ON' });
+    }
+    catch (e){
+        res.json({res: e})
+    }
+
+
+
 });
 
 module.exports = app;
